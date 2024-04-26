5e9189b4781a7fd591001bb7bb45af316f8230e7
HIMDanny <danilbliz0201@gmail.com>
2024-04-26 18:25:49 +0300

    Handle highlighted keys on key press

---
+++ README.md
@@ -0,0 +1,5 @@
+## Tasks
+
+- [x] Display a keyboard
+- [x] Highlight letter on key press
+- [x] Print letter on key press
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
+import { useEffect, useRef, useState } from "react";
+import { Keyboard } from "./components/keyboard";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold">Hello</h1>;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [highlightedKeys, setHighlightedKeys] = useState<Set<string>>(
+    new Set(),
+  );
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      inputRef?.current?.focus();
+
+      setHighlightedKeys((prevHighlightedKeys) => {
+        const newHighlightedKeys = new Set(prevHighlightedKeys);
+        newHighlightedKeys.add(event.code);
+
+        return newHighlightedKeys;
+      });
+    };
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      setHighlightedKeys((prevHighlightedKeys) => {
+        const newHighlightedKeys = new Set(prevHighlightedKeys);
+        newHighlightedKeys.delete(event.code);
+
+        return newHighlightedKeys;
+      });
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
+  return (
+    <main className="grid min-h-screen place-items-center">
+      <div className="grid gap-10">
+        <input
+          ref={inputRef}
+          className="min-h-9 text-center text-3xl font-bold focus:outline-none"
+        />
+        <Keyboard highlightedKeys={highlightedKeys} />
+      </div>
+    </main>
+  );
 };
 
 export default App;
